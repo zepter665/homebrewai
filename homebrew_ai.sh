efbdf9589864c8cf2e00c4898249b649a5c5a03a
zepter665 <thedodger666@gmx.de>
2026-04-04 21:36:38 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	renamed:    conntrack -> binaries/conntrack
    	deleted:    cni/bin/LICENSE
    	deleted:    cni/bin/README.md
    	deleted:    cni/bin/bandwidth
    	deleted:    cni/bin/bridge
    	deleted:    cni/bin/dhcp
    	deleted:    cni/bin/dummy
    	deleted:    cni/bin/firewall
    	deleted:    cni/bin/host-device
    	deleted:    cni/bin/host-local
    	deleted:    cni/bin/ipvlan
    	deleted:    cni/bin/loopback
    	deleted:    cni/bin/macvlan
    	deleted:    cni/bin/portmap
    	deleted:    cni/bin/ptp
    	deleted:    cni/bin/sbr
    	deleted:    cni/bin/static
    	deleted:    cni/bin/tap
    	deleted:    cni/bin/tuning
    	deleted:    cni/bin/vlan
    	deleted:    cni/bin/vrf
    	deleted:    cri-dockerd
    	deleted:    crictl
    	deleted:    docker/containerd
    	deleted:    docker/containerd-shim-runc-v2
    	deleted:    docker/ctr
    	deleted:    docker/docker
    	deleted:    docker/docker-init
    	deleted:    docker/docker-proxy
    	deleted:    docker/dockerd
    	deleted:    docker/runc
    	deleted:    helm
    	modified:   homebrew_ai.sh
    	deleted:    kubectl
    	renamed:    containerd.service -> services/containerd.service
    	renamed:    cri-docker.service -> services/cri-docker.service
    	renamed:    cri-docker.socket -> services/cri-docker.socket
    	renamed:    docker.service -> services/docker.service
    	renamed:    docker.socket -> services/docker.socket

--- homebrew_ai.sh
+++ homebrew_ai.sh
@@ -1,43 +1,18 @@
-### PREREQUIREMENTS ###
-
-# wget https://download.docker.com/linux/static/stable/x86_64/docker-29.3.1.tgz
-# sudo tar zxvf docker-29.3.1.tgz
-# sudo rm -f docker-29.3.1.tgz
-
-# apt install conntrack -y
-
-# VERSION="v1.31.0" # check latest version in /releases page
-# wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
-# sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
-# rm -f crictl-$VERSION-linux-amd64.tar.gz
-
-# wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.4.2/cri-dockerd-0.4.2.amd64.tgz
-# sudo tar zxvf cri-dockerd-0.4.2.amd64.tgz
-
-# CNI_PLUGIN_VERSION="v1.9.1"
-# CNI_PLUGIN_TAR="cni-plugins-linux-amd64-$CNI_PLUGIN_VERSION.tgz" # change arch if not on amd64
-# CNI_PLUGIN_INSTALL_DIR="/opt/cni/bin"
-
-# curl -LO "https://github.com/containernetworking/plugins/releases/download/$CNI_PLUGIN_VERSION/$CNI_PLUGIN_TAR"
-# sudo mkdir -p "$CNI_PLUGIN_INSTALL_DIR"
-# sudo tar -xf "$CNI_PLUGIN_TAR" -C "$CNI_PLUGIN_INSTALL_DIR"
-# rm "$CNI_PLUGIN_TAR"
-
-# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
-# wget https://get.helm.sh/helm-v4.1.3-linux-amd64.tar.gz
-# sudo tar zxvf helm-v4.1.3-linux-amd64.tar.gz
-
 #######################################
-
 # apt install docker.io -y
+
+## Docker Runtime Install
+wget https://download.docker.com/linux/static/stable/x86_64/docker-29.3.1.tgz
+tar zxvf docker-29.3.1.tgz
 sudo cp docker/* /usr/bin/
+rm ./docker -R
+rm docker-29.3.1.tgz
 sudo groupadd docker 2>&1
 sudo mkdir /etc/docker
-
-install containerd.service /etc/systemd/system
-install docker.service /etc/systemd/system
-install docker.socket /etc/systemd/system
+# Daemon Install
+install ./services/containerd.service /etc/systemd/system
+install ./services/docker.service /etc/systemd/system
+install ./services/docker.socket /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable containerd
 sudo systemctl start containerd
@@ -45,38 +20,63 @@ sudo systemctl enable docker.socket
 sudo systemctl enable docker.service
 sudo systemctl start docker
 
-sudo cp conntrack /usr/sbin/conntrack
+## Conntrack Install
+sudo cp ./binaries/conntrack /usr/sbin/conntrack
 sudo chmod 755 /usr/sbin/conntrack
 
+## Container Runtime Interface Control (crictl) Install
+VERSION="v1.31.0" # check latest version in /releases page
+wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
+tar zxvf crictl-$VERSION-linux-amd64.tar.gz
 sudo cp crictl /usr/local/bin/crictl
 sudo chmod 755 /usr/local/bin/crictl
+rm -f ./crictl
+rm -f crictl-$VERSION-linux-amd64.tar.gz
 
-sudo cp cni /opt/ -R
-sudo chmod 755 /opt/cni/bin/* -R
+## Container Network Interface (cni) Install
+CNI_PLUGIN_VERSION="v1.9.1"
+CNI_PLUGIN_TAR="cni-plugins-linux-amd64-$CNI_PLUGIN_VERSION.tgz" # change arch if not on amd64
+CNI_PLUGIN_INSTALL_DIR="/opt/cni/bin"
+wget "https://github.com/containernetworking/plugins/releases/download/$CNI_PLUGIN_VERSION/$CNI_PLUGIN_TAR"
+sudo mkdir -p "$CNI_PLUGIN_INSTALL_DIR"
+sudo tar -xf "$CNI_PLUGIN_TAR" -C "$CNI_PLUGIN_INSTALL_DIR"
+rm "$CNI_PLUGIN_TAR"
 
-install -o root -g root -m 0755 cri-dockerd /usr/local/bin/cri-dockerd
-install cri-docker.service /etc/systemd/system
-install cri-docker.socket /etc/systemd/system
+## Container Runtime Interface dockerd (cri-dockerd) Install
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.4.2/cri-dockerd-0.4.2.amd64.tgz
+sudo tar zxvf cri-dockerd-0.4.2.amd64.tgz
+install -o root -g root -m 0755 ./cri-dockerd/cri-dockerd /usr/local/bin/cri-dockerd
+install ./services/cri-docker.service /etc/systemd/system
+install ./services/cri-docker.socket /etc/systemd/system
 sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
 systemctl daemon-reload
 systemctl enable --now cri-docker.socket
-
 service cri-docker start
+rm ./cri-dockerd -R
+rm cri-dockerd-0.4.2.amd64.tgz
 
+## MiniKube Install
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-
 minikube start --driver=none --cni calico
 sudo minikube config set driver none
 
-# kubectl
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-# HELM
-install -o root -g root -m 0755 helm /usr/local/bin/helm
+## kubectl Install
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm ./kubectl
 
-# Kube-Config für externe Verwendung anpassen
+## HELM  Install
+wget https://get.helm.sh/helm-v4.1.3-linux-amd64.tar.gz
+tar zxvf helm-v4.1.3-linux-amd64.tar.gz
+sudo install -o root -g root -m 0755 ./linux-amd64/helm /usr/local/bin/helm
+rm ./helm-v4.1.3-linux-amd64.tar.gz
+rm ./linux-amd64 -R
+
+## Kube-Config für externe Verwendung anpassen
 chmod +x ./embed_kubeconfig_certs.sh 
 ./embed_kubeconfig_certs.sh 
+
 ##################################################################
 ## KI installieren
 
