5786af2d05dcc7c3563c992ee7ce1c875db72916
zepter665 <thedodger666@gmx.de>
2026-07-14 23:55:58 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   homebrew_ai.sh

--- homebrew_ai.sh
+++ homebrew_ai.sh
@@ -11,17 +11,15 @@ install ./services/containerd.service /etc/systemd/system
 install ./services/docker.service /etc/systemd/system
 install ./services/docker.socket /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable containerd
-sudo systemctl start containerd
+sudo systemctl enable containerd && sudo systemctl start containerd
 sudo systemctl enable docker.socket
-sudo systemctl enable docker.service
-sudo systemctl start docker
+sudo systemctl enable docker.service && sudo systemctl start docker
 
 ## Conntrack Install > conntrack (Connection Tracking) ist ein Bestandteil des Linux-Kernels (Netfilter), der Netzwerkverbindungen verfolgt und ihren Zustand speichert
 sudo cp ./binaries/conntrack /usr/sbin/conntrack
 sudo chmod 755 /usr/sbin/conntrack
 
-## Container Runtime Interface Control (crictl) Install
+## Container Runtime Interface Control (crictl) Install > crictl ist ein Kommandozeilenwerkzeug zur Fehleranalyse und Verwaltung von Container-Workloads über die Container Runtime Interface (CRI)
 VERSION="v1.31.0" # check latest version in /releases page
 wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
 tar zxvf crictl-$VERSION-linux-amd64.tar.gz
@@ -39,7 +37,7 @@ sudo mkdir -p "$CNI_PLUGIN_INSTALL_DIR"
 sudo tar -xf "$CNI_PLUGIN_TAR" -C "$CNI_PLUGIN_INSTALL_DIR"
 rm "$CNI_PLUGIN_TAR"
 
-## Container Runtime Interface dockerd (cri-dockerd) Install
+## Container Runtime Interface dockerd (cri-dockerd) Install > cri-dockerd ist ein Adapterdienst, der Docker mit der Kubernetes Container Runtime Interface (CRI) verbindet und so die Nutzung von Docker als Container-Runtime in Kubernetes ermöglicht
 wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.4.2/cri-dockerd-0.4.2.amd64.tgz
 sudo tar zxvf cri-dockerd-0.4.2.amd64.tgz
 install -o root -g root -m 0755 ./cri-dockerd/cri-dockerd /usr/local/bin/cri-dockerd
@@ -52,27 +50,28 @@ service cri-docker start
 rm ./cri-dockerd -R
 rm cri-dockerd-0.4.2.amd64.tgz
 
-## MiniKube Install
+## MiniKube Install > Minikube ist ein Werkzeug, das einen lokalen Kubernetes-Cluster auf einem einzelnen Rechner erstellt und verwaltet, um Kubernetes für Entwicklung, Tests und Schulungen einfach nutzbar zu machen
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 minikube start --driver=none --cni calico
 sudo minikube config set driver none
 
-## kubectl Install
+## kubectl Install > kubectl ist ein Kommandozeilenwerkzeug zur Verwaltung von Kubernetes-Clustern
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm ./kubectl
-## HELM  Install
+## HELM  Install > HELM ist ein Paketmanager für Kubernetes, der die Verwaltung von Kubernetes-Anwendungen vereinfacht
 wget https://get.helm.sh/helm-v4.1.3-linux-amd64.tar.gz
 tar zxvf helm-v4.1.3-linux-amd64.tar.gz
 sudo install -o root -g root -m 0755 ./linux-amd64/helm /usr/local/bin/helm
 rm ./helm-v4.1.3-linux-amd64.tar.gz
 rm ./linux-amd64 -R
-
 ## Kube-Config für externe Verwendung anpassen  !!! ab hier den Verlauf in FreeLens zeigen
 chmod +x ./embed_kubeconfig_certs.sh && ./embed_kubeconfig_certs.sh
-##################################################################
-## KI installieren
+
+###### KI installieren ##################
+# LLM manuell kopieren, wegen Zeitersparnis
+mkdir -p /tmp/hostpath-provisioner/default && cp -R /tmp/pv-ollama /tmp/hostpath-provisioner/default/ollama
 # Ollama installieren (HELM)
 helm repo add otwld "https://helm.otwld.com/"
 helm install ollama otwld/ollama \
@@ -82,7 +81,8 @@ helm install ollama otwld/ollama \
 --set ollama.models.run={mistral} \
 --set persistentVolume.enabled=false \
 --set service.type=NodePort \
---set service.nodePort=30667
+--set service.nodePort=30667 \
+--set persistentVolume.enabled=true \
 # OpenWebUI installieren (HELM)
 helm repo add open-webui "https://helm.openwebui.com/"
 helm install open-webui open-webui/open-webui \
@@ -101,3 +101,7 @@ helm install open-webui open-webui/open-webui \
 
 
 
+
+############################################################
+# Helper
+code /root/.kube/config 
